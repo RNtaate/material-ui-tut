b5327f6b438b2eb80ab2311cbc7744585d7d9f56
RNtaate <royhyde14@gmail.com>
2021-10-01 15:11:41 +0300

    Install react-router-dom dependency
    Use dependency in App.js to display Notes, Create pages.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
 
 function App() {
   return (
-    <div >
-      <h1>My Project</h1>
-      <Notes />
-      <Create />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
