2b8613eff183c61e14cd6a77282313665ff68527
RNtaate <royhyde14@gmail.com>
2021-10-01 19:23:58 +0300

    Install Material UI, svg icons
    Use the installed dependencies in Create.js file

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import SendIcon from '@mui/icons-material/Send';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const Create = () => {
   return (
-    <div>
-      <h2>This is the create component.</h2>
-    </div>
+    <Container>
+      <Typography variant="h6" component="h2" gutterBottom color="textSecondary">
+        Create a New Note
+      </Typography>
+
+      <Button 
+        variant="contained"
+        color="primary"
+        startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon/>}
+      >
+          Submit
+      </Button>
+    </Container>
   )
 }
 
