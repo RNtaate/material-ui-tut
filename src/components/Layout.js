48960a5d09da7ef44590edd4e0dc07db96610bf0
RNtaate <royhyde14@gmail.com>
2021-10-11 11:22:20 +0300

    Create Side bar using Drawer component
    Add custom styling to components in Layout using makeStyles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import Typography from '@mui/material/Typography';
+import Drawer from '@mui/material/Drawer';
+
+const drawerWidth = 240;
+
+let useStyles = makeStyles({
+  root: {
+    display: "flex"
+  },
+  page: {
+    backgroundColor: "#f9f9f9",
+    width: "100%"
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  }
+});
 
 let Layout = ({ children }) => {
+
+  let classes = useStyles();
   return (
-    <div>
-      <h2>Layout</h2>
-      {children}
+    <div className={classes.root}>
+
+      {/* Side Bar*/}
+
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{paper: classes.drawerPaper}}
+      >
+        <div>
+          <Typography variant="h5" color="text.secondary">
+            Norp Notes
+          </Typography>
+        </div>
+      </Drawer>
+
+      <div className={classes.page}>
+        {children}
+      </div>
     </div>
   )
 }
