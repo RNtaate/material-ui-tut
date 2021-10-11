38e2b6ff98a2f5eb8777afbb4e2e0e7916e5a1a8
RNtaate <royhyde14@gmail.com>
2021-10-11 16:15:42 +0300

    Add ListItemButtons to sidbar
    Implement navigation to other pages using added listItem components
    
    Use useLocation hook to achieve navigation to other pages.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { makeStyles } from '@mui/styles';
 import Typography from '@mui/material/Typography';
 import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@mui/icons-material';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -18,27 +24,74 @@ let useStyles = makeStyles({
   },
   drawerPaper: {
     width: drawerWidth,
+  },
+  drawerButton: {
+    backgroundColor: "#f3f3f3"
   }
 });
 
 let Layout = ({ children }) => {
 
   let classes = useStyles();
+  let history = useHistory();
+  let location = useLocation();
+
+  const menuItems = [
+    {
+      text: "My Notes",
+      icon: <SubjectOutlined color="secondary"/>,
+      path: "/"
+    },
+    {
+      text: "Create Note",
+      icon: <AddCircleOutlineOutlined color="secondary"/>,
+      path: "/create"
+    }
+  ]
+
+  let handlePathClick = (item) => {
+    history.push(item.path);
+  }
+
   return (
     <div className={classes.root}>
 
       {/* Side Bar*/}
 
       <Drawer
-        className={classes.drawer}
+        sx={{
+          width: drawerWidth
+        }}
         variant="permanent"
         anchor="left"
         classes={{paper: classes.drawerPaper}}
       >
         <div>
-          <Typography variant="h5" color="text.secondary">
+          <Typography 
+            variant="h5"
+            color="text.secondary"
+            sx={{
+              padding: (theme) => theme.spacing(2)
+            }}
+          >
             Norp Notes
           </Typography>
+
+          {/** List links */}
+          <List>
+            {menuItems.map(item => (
+              <ListItemButton
+                key={item.text}
+                onClick={ () => handlePathClick(item)}
+                sx={{
+                  backgroundColor: location.pathname == item.path ? "#f3f3f3" : null
+                }}
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text}/>
+              </ListItemButton>
+            ))}
+          </List>
         </div>
       </Drawer>
 
