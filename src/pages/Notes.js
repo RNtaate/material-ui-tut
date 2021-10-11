dd8005792fa6ca5991cdd821b79bc7c75aa312c0
RNtaate <royhyde14@gmail.com>
2021-10-11 18:40:38 +0300

    Use react-masonry-css dependency in Notes.js
    Use the dependency to properly stack and arrange grid items

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,11 +1,17 @@
 import { Container, Typography } from '@mui/material';
 import React, { useState, useEffect } from 'react'
 import Grid from '@mui/material/Grid';
+import Masonry from 'react-masonry-css'
 
 import NoteCard from '../components/NoteCard';
 
 const Notes = () =>  {
   let [myNotes, setMyNotes] = useState([]);
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
+  }
 
   let handleNoteDelete = async (note) => {
     await fetch("http://localhost:8000/notes/" + note.id, {
@@ -37,15 +43,19 @@ const Notes = () =>  {
     <Container 
       sx={{padding: 2}}
     >
-      <Grid container spacing={2}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {
           myNotes.map(note => (
-            <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <div key={note.id} >
               <NoteCard note={note} handleNoteDelete={handleNoteDelete}/>
-            </Grid>
+            </div>
           ))
         }
-      </Grid>
+      </Masonry>
     </Container>
   )
 }
